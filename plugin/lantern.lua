9b3e33a29b427dc711fc24bf92d2863a5b6b5419
Josh Petrie <josh@joshpetrie.net>
2026-05-15 21:34:18 -0700

    Add Config and Target commands.

--- plugin/lantern.lua
+++ plugin/lantern.lua
@@ -5,61 +5,48 @@ local function filter_options(options, prefix)
   return vim.iter(options):filter(function(option) return option:find(prefix) ~= nil end):totable()
 end
 
-local commands = {
-  config = {
-    callback = function(arguments) require("lantern").set_configuration(arguments[1]) end,
-    completion = function(prefix)
-      local options = vim.tbl_keys(require("lantern").project().configurations)
-      return filter_options(options, prefix)
-    end
-  },
 
-  target = {
-    callback = function(arguments) require("lantern").set_target(arguments[1]) end,
-    completion = function(prefix)
-      local configuration = require("lantern").configuration()
-      if configuration ~= nil then
-        local options = vim.tbl_keys(configuration.targets)
-        return filter_options(options, prefix)
-      end
-
-      -- There are no targets available if no configuration is active.
-      return {}
-    end
-  },
-}
-
-local function lantern_command(command)
-  local arguments = command.fargs
-  local action = arguments[1]
-  local definition = commands[action]
-  if definition ~= nil and definition.callback ~= nil then
-    definition.callback(vim.list_slice(arguments, 2, #arguments))
-  else
-    vim.notify("Lantern: Unknown action: " .. action, vim.log.levels.ERROR)
-  end
+local function command_config(command)
+  local lantern = require("lantern")
+  lantern.set_configuration(command.fargs[1])
 end
 
-local function lantern_complete(prefix, command, _)
-  local action, argument_prefix = string.match(command, "^['<,'>]*Lantern[!]*%s(%S+)%s(.*)$")
-  if action ~= nil and argument_prefix ~= nil then
-    local entry = commands[action]
-    if entry ~= nil and entry.completion ~= nil then
-      return entry.completion(argument_prefix)
-    end
+local function complete_config(prefix)
+  local project = require("lantern").project()
+  if project ~= nil then
+    local options = vim.tbl_keys(project.configurations)
+    return filter_options(options, prefix)
   end
 
-  if string.match(command, "^['<,'>]*Lantern[!]*%s+%w*$") then
-    local keys = vim.tbl_keys(commands)
-    return filter_options(keys, prefix)
-  end
-
+  -- There are no configurations available if no project is active.
   return {}
 end
 
-vim.api.nvim_create_user_command("Lantern", lantern_command, {
-  nargs = "+",
-  complete = lantern_complete,
-  desc = "Control Lantern",
+local function command_target(command)
+  local lantern = require("lantern")
+  lantern.set_target(command.fargs[1])
+end
+
+local function complete_target(prefix)
+  local configuration = require("lantern").configuration()
+  if configuration ~= nil then
+    local options = vim.tbl_keys(configuration.targets)
+    return filter_options(options, prefix)
+  end
+
+  -- There are no targets available if no configuration is active.
+  return {}
+end
+
+vim.api.nvim_create_user_command("Config", command_config, {
+  nargs = 1,
+  complete = complete_config,
+  desc = "Lantern: set configuration"
+})
+
+vim.api.nvim_create_user_command("Target", command_target, {
+  nargs = 1,
+  complete = complete_target,
+  desc = "Lantern: set target"
 })
 
